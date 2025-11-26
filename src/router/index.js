68661cdc47b94d810fb70183f4998acfb4c2fcb9
Katarine-meira <katarine.kmo@gmail.com>
2025-11-26 12:03:14 -0300

    feat: update

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import {
   createWebHashHistory,
 } from 'vue-router'
 import routes from './routes'
+import { useAuthStore } from 'src/store/auth'
+import { jwtDecode } from 'jwt-decode'
 
 /*
  * If not building with SSR mode, you can
@@ -32,6 +34,38 @@ export default defineRouter(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE),
   })
+
+  Router.beforeEach((to, from, next) => {
+    const auth = useAuthStore()
+
+    // 1) Verifica expiração do token
+    if (auth.token) {
+      try {
+        const decoded = jwtDecode(auth.token)
+        if (decoded.exp * 1000 < Date.now()) {
+          auth.logout()
+          return next('/auth')
+        }
+      } catch (error) {
+        console.log(error);
+        auth.logout()
+        return next('/auth')
+      }
+    }
+
+    // 2) Rotas que exigem autenticação
+    if (to.meta.requiresAuth && !auth.isAuthenticated) {
+      return next('/auth')
+    }
+
+    // 3) Rotas que exigem permissões específicas
+    if (to.meta.requiresAdmin && auth.userRole !== 'admin') {
+      return next('/unauthorized') // você cria essa página
+    }
+
+    next()
+  })
+
 
   return Router
 })
