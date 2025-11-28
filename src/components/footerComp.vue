6eed4ecd15f131ca2a752427706678ede952b79f
Katarine-meira <katarine.kmo@gmail.com>
2025-11-28 11:57:42 -0300

    feat: update homePage

---
+++ src/components/footerComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <q-footer elevated class="color_footer">
+        <q-toolbar>
+          <q-toolbar-title class="text-4 text-center q-pa-sm">Liga Acadêmica de Tecnologia &copy; {{ano_atual}}</q-toolbar-title>
+        </q-toolbar>
+    </q-footer>
+</template>
+
+<script setup>
+    var ano_atual = new Date().getFullYear()
+</script>
+
+<style scoped>
+.color_footer{
+    background-color: #1a112b;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,26 +1,53 @@
 <template>
     <div class="fundo_site q-pa-none">
-        <h1 class="q-ma-none"></h1>
-       
+        <q-img
+        src="../assets/bannerLitec.png"
+        style="height: 400px; width: 100%;"
+        fit="cover"
+        />
+        <div>
+        <h1 class="q-ml-xl text-h3">SITES DA INSTITUIÇÃO</h1>
+        <div class="row">
+            <div class="col-6">
+
+            </div>
+            <div class="col-6">
+                
+            </div>
+        </div>
+        </div>
     </div>
-    <q-footer elevated class="color_footer">
-        <q-toolbar>
-          <q-toolbar-title class="text-4 text-center q-pa-sm">Liga Acadêmica de Tecnologia &copy; {{ano_atual}}</q-toolbar-title>
-        </q-toolbar>
-    </q-footer>
+    <footer-comp/>
 </template>
 
 <script setup>
-    var ano_atual = new Date().getFullYear()
+import footerComp from 'src/components/footerComp.vue';
+
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald:wght@200..700&family=Roboto+Flex:opsz,wght@8..144,600&display=swap');
+*{
+  font-family: "Roboto Flex", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+  font-variation-settings:
+    "slnt" 0,
+    "wdth" 100,
+    "GRAD" 0,
+    "XOPQ" 96,
+    "XTRA" 468,
+    "YOPQ" 79,
+    "YTAS" 750,
+    "YTDE" -203,
+    "YTFI" 738,
+    "YTLC" 514,
+    "YTUC" 712;
+}
 .fundo_site{
     background-color: #ece8e1;
     min-height: 90vh;
     padding: 0px;
-}
-.color_footer{
-    background-color: #1a112b;
 }
 </style>
